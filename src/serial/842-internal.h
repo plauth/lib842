a8fa40593bb513a6b8e970c98e6f71aa80028328
Max Plauth <max.plauth@hpi.de>
2018-03-12 11:19:07 +0100

    restored non-optimized version

--- src/serial/842-internal.h
+++ src/serial/842-internal.h
@@ -137,21 +137,36 @@
 /* the max of the regular templates - not including the special templates */
 #define OPS_MAX		(0x1a)
 
+struct node2_el {
+    uint8_t index;
+    struct node2_el *next, *prev;
+};
+
 struct hlist_node2 {
 	uint16_t data;
-	uint8_t index;
+	struct node2_el *head;
 	UT_hash_handle hh;
+};
+
+struct node4_el {
+    uint16_t index;
+    struct node4_el *next, *prev;
 };
 
 struct hlist_node4 {
 	uint32_t data;
-	uint16_t index;
+	struct node4_el *head;
 	UT_hash_handle hh;
+};
+
+struct node8_el {
+    uint8_t index;
+    struct node8_el *next, *prev;
 };
 
 struct hlist_node8 {
 	uint64_t data;
-	uint8_t index;
+	struct node8_el *head;
 	UT_hash_handle hh;
 };
 
