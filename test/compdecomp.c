5d65360080635e10318467e7c4b5c34ed7221ce7
Max Plauth <max.plauth@hpi.de>
2018-05-08 10:00:59 +0200

    added condensation step

--- test/compdecomp.c
+++ test/compdecomp.c
@@ -32,6 +32,7 @@ int main( int argc, const char* argv[])
 	ilen = olen = dlen = 0;
 	long long timestart_comp, timeend_comp;
 	long long timestart_decomp, timeend_decomp;
+	long long timestart_condense, timeend_condense;
 
 	if(argc <= 1) {
 		ilen = 32;
@@ -85,13 +86,13 @@ int main( int argc, const char* argv[])
 
 	if(ilen > CHUNK_SIZE) {
 		printf("Using chunks of %d bytes\n", CHUNK_SIZE);
-		unsigned int acc_olen = 0;
-		
-
+	
 		int num_chunks = ilen / CHUNK_SIZE;
+		uint64_t *compressedChunkPositions = (uint64_t*) malloc(sizeof(uint64_t) * num_chunks);
+		uint32_t *compressedChunkSizes = (uint32_t*) malloc(sizeof(uint32_t) * num_chunks);
 
 		timestart_comp = timestamp();
-		#pragma omp parallel for reduction(+ : acc_olen)
+		#pragma omp parallel for
 		for(int chunk_num = 0; chunk_num < num_chunks; chunk_num++) {
 			
 			unsigned int chunk_olen = CHUNK_SIZE * 2;
@@ -103,12 +104,27 @@ int main( int argc, const char* argv[])
 			#else
 			sw842_compress(chunk_in, CHUNK_SIZE, chunk_out, &chunk_olen);
 			#endif
-			acc_olen += chunk_olen;
+			compressedChunkSizes[chunk_num] = chunk_olen;
 		}
 		timeend_comp = timestamp();
-		
-		int chunk_olen = CHUNK_SIZE * 2;
+		timestart_condense = timeend_comp;
 
+		uint64_t currentChunkPos = 0;
+		for(int chunk_num = 0; chunk_num < num_chunks; chunk_num++) {
+			compressedChunkPositions[chunk_num] = currentChunkPos;
+			currentChunkPos += compressedChunkSizes[chunk_num];
+		}
+
+		uint8_t *out_condensed = (uint8_t *) malloc(currentChunkPos);
+
+		#pragma omp parallel for
+		for(int chunk_num = 0; chunk_num < num_chunks; chunk_num++) {
+			uint8_t * chunk_out = out + ((CHUNK_SIZE * 2) * chunk_num);
+			uint8_t * chunk_condensed = out_condensed + compressedChunkPositions[chunk_num];
+			memcpy(chunk_condensed, chunk_out, compressedChunkSizes[chunk_num]);
+		}
+		timeend_condense = timestamp();
+		
 
 		timestart_decomp = timestamp();
 		#pragma omp parallel for
@@ -116,14 +132,14 @@ int main( int argc, const char* argv[])
 			unsigned int chunk_dlen = CHUNK_SIZE;
 
 			uint8_t* chunk_in = in + (CHUNK_SIZE * chunk_num);
-			uint8_t* chunk_out = out + ((CHUNK_SIZE * 2) * chunk_num);
+			uint8_t* chunk_condensed = out_condensed + compressedChunkPositions[chunk_num];
 			uint8_t* chunk_decomp = in + (CHUNK_SIZE * chunk_num);
 			
 			
 			#ifdef USEHW
-			hw842_decompress(chunk_out, chunk_olen, chunk_decomp, &chunk_dlen);
+			hw842_decompress(chunk_condensed, compressedChunkSizes[chunk_num], chunk_decomp, &chunk_dlen);
 			#else
-			sw842_decompress(chunk_out, chunk_olen, chunk_decomp, &chunk_dlen);
+			sw842_decompress(chunk_condensed, compressedChunkSizes[chunk_num], chunk_decomp, &chunk_dlen);
 			#endif
 
 			if (!(memcmp(chunk_in, chunk_decomp, CHUNK_SIZE) == 0)) {
@@ -133,10 +149,14 @@ int main( int argc, const char* argv[])
 		}
 		timeend_decomp = timestamp();
 
+		free(compressedChunkPositions);
+		free(compressedChunkSizes);
+
 		printf("Input: %d bytes\n", ilen);
-		printf("Output: %d bytes\n", acc_olen);
-		printf("Compression factor: %f\n", (float) acc_olen / (float) ilen);
+		printf("Output: %lld bytes\n", currentChunkPos);
+		printf("Compression factor: %f\n", (float) currentChunkPos / (float) ilen);
 		printf("Compression performance: %lld ms / %f MiB/s\n", timeend_comp - timestart_comp, (ilen / 1024 / 1024) / ((float) (timeend_comp - timestart_comp) / 1000));
+		printf("Condensation performance: %lld ms / %f MiB/s\n", timeend_condense - timestart_condense, (currentChunkPos / 1024 / 1024) / ((float) (timeend_condense - timestart_condense) / 1000));
 		printf("Decompression performance: %lld ms / %f MiB/s\n", timeend_decomp - timestart_decomp, (ilen / 1024 / 1024) / ((float) (timeend_decomp - timestart_decomp) / 1000));
 
 		printf("Compression- and decompression was successful!\n");
