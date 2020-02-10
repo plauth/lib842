6bc6282bc7332272a2522d02eeec296e3344e598
Joan Bruguera <joanbrugueram@gmail.com>
2020-02-10 22:18:52 +0100

    Add 'extern C' markers to the include headers, so the implementation can be used from both C and C++, and also to avoid the serial optimized version from using C++ name manging so it can be used from C.

--- include/sw842.h
+++ include/sw842.h
@@ -3,11 +3,18 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 int sw842_compress(const uint8_t *in, size_t ilen,
 		   uint8_t *out, size_t *olen);
 
 int sw842_decompress(const uint8_t *in, size_t ilen,
 		     uint8_t *out, size_t *olen);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
