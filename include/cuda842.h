ee392a8d9cd2709d5b81eab593f30a691aa39159
Joan Bruguera <joanbrugueram@gmail.com>
2020-03-22 02:16:43 +0100

    Remove unnecessary ifndef for (CUDA|CL)842_CHUNK_SIZE.

--- include/cuda842.h
+++ include/cuda842.h
@@ -3,10 +3,7 @@
 
 #include <stdint.h>
 
-#ifndef CUDA842_CHUNK_SIZE
 #define CUDA842_CHUNK_SIZE 1024
-#endif
-
 #define CUDA842_STRICT
 
 __global__ void cuda842_decompress(uint64_t *in, uint64_t *out);
