f481e79408cfadc1d2922ca73581d01227dc7a1d
Joan Bruguera <joanbrugueram@gmail.com>
2020-05-17 10:28:17 +0200

    Set CL_TARGET_OPENCL_VERSION macro for new versions of opencl-headers + define macros in the build system and not the source files.

--- include/lib842/cl.h
+++ include/lib842/cl.h
@@ -7,9 +7,6 @@
 
 #ifdef __cplusplus
 
-#define CL_HPP_MINIMUM_OPENCL_VERSION 120
-#define CL_HPP_TARGET_OPENCL_VERSION 120
-#define CL_HPP_ENABLE_EXCEPTIONS
 #ifdef __APPLE__
 #include <OpenCL/cl2.hpp>
 #else
