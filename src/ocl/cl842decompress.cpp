plauth/lib842

commit e17a42c57aaa8e65e057a82c984878cca423fd2b

Various general code quality improvements (possibly with performance impact): Make definitions more local, use restrict where possible, small style fixes, etc.

src/ocl/cl842decompress.cpp
@@ -1,10 +1,12 @@
 #include <lib842/cl.h>
+#include <lib842/common.h>
 
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <chrono>
 #include <algorithm>
+#include <iterator>
 
 #define LIB842_CLDECOMPRESS_USE_PROGRAM_CACHE
 
@@ -236,6 +238,13 @@ void CLDeviceDecompressor::buildProgram(
 	options << " -D CL842_CHUNK_STRIDE=" << m_inputChunkStride;
 	options << " -D EINVAL=" << EINVAL;
 	options << " -D ENOSPC=" << ENOSPC;
+	if (m_inputFormat == CLDecompressorInputFormat::MAYBE_COMPRESSED_CHUNKS) {
+		options << " -D LIB842_COMPRESSED_CHUNK_MARKER_DEF={";
+		std::copy(std::begin(LIB842_COMPRESSED_CHUNK_MARKER),
+			  std::end(LIB842_COMPRESSED_CHUNK_MARKER),
+			  std::ostream_iterator<double>(options, ","));
+		options << "}";
+	}
 	if (m_inputFormat == CLDecompressorInputFormat::MAYBE_COMPRESSED_CHUNKS)
 		options << " -D USE_MAYBE_COMPRESSED_CHUNKS=1";
 	else if (m_inputFormat == CLDecompressorInputFormat::INPLACE_COMPRESSED_CHUNKS)

include/lib842/cuda.h
@@ -10,7 +10,8 @@
 #define LIB842_CUDA_CHUNK_SIZE 1024
 #define LIB842_CUDA_STRICT
 
-__global__ void cuda842_decompress(const uint64_t *in, uint64_t *out);
+__global__ void cuda842_decompress(__restrict__ const uint64_t *in,
+				   __restrict__ uint64_t *out);
 
 #endif
 

sample/compdecomp_streams.cpp
@@ -201,7 +201,7 @@ bool compress_benchmark_core(const uint8_t *in, size_t ilen,
 			} else {
 				// TODOXXX: Should this be done multi thread???
 				//          Or maybe done separately and not even included in the
-				//          timing, since that's usually handled by the network? 
+				//          timing, since that's usually handled by the network?
 				memcpy(dest, cblock.datas[i], lib842::stream::CHUNK_SIZE);
 			}
 		}

src/cuda/842-internal.h
@@ -15,12 +15,4 @@
 #define I8_BITS (STATIC_LOG2_VALUE - 3)
 #endif
 
-struct sw842_param_decomp {
-	uint64_t *out;
-	const uint64_t *ostart;
-	const uint64_t *in;
-	uint32_t bits;
-	uint64_t buffer;
-};
-
 #endif // LIB842_SRC_CUDA_842_INTERNAL_H

src/cuda/842_decompress.cu
@@ -1,5 +1,13 @@
 #include "842-internal.h"
 
+struct sw842_param_decomp {
+	uint64_t *out;
+	const uint64_t *ostart;
+	const uint64_t *in;
+	uint32_t bits;
+	uint64_t buffer;
+};
+
 /* number of bits in a buffered word */
 #define WSIZE 64 //sizeof(uint64_t)
 
@@ -12,10 +20,11 @@
 #define __round_mask(x, y) ((__typeof__(x))((y)-1))
 #define round_down(x, y) ((x) & ~__round_mask(x, y))
 
-__constant__ uint16_t fifo_sizes[9] = { 0, 0, I2_FIFO_SIZE, 0, I4_FIFO_SIZE, 0, 0, 0, I8_FIFO_SIZE };
+__constant__ static const uint16_t fifo_sizes[9] = { 0, 0, I2_FIFO_SIZE, 0, I4_FIFO_SIZE, 0,
+						     0, 0, I8_FIFO_SIZE };
 #endif
 
-__constant__ uint8_t dec_templates[26][4][2] = {
+__constant__ static const uint8_t dec_templates[26][4][2] = {
 	// params size in bits
 	{ OP_DEC_D8, OP_DEC_N0, OP_DEC_N0, OP_DEC_N0 }, // 0x00: { D8, N0, N0, N0 }, 64 bits
 	{ OP_DEC_D4, OP_DEC_D2, OP_DEC_I2, OP_DEC_N0 }, // 0x01: { D4, D2, I2, N0 }, 56 bits
@@ -51,7 +60,7 @@ __constant__ uint8_t dec_templates[26][4][2] = {
 	{ OP_DEC_I8, OP_DEC_N0, OP_DEC_N0, OP_DEC_N0 }, // 0x19: { I8, N0, N0, N0 }, 8 bits
 };
 
-__device__ inline uint64_t bswap(uint64_t value)
+__device__ static inline uint64_t bswap(uint64_t value)
 {
 	asm("{\n\t"
 	    "		.reg .b32 %li,%lo,%hi,%ho;\n\t"
@@ -64,7 +73,7 @@ __device__ inline uint64_t bswap(uint64_t value)
 	return value;
 }
 
-__device__ inline uint64_t read_bits(struct sw842_param_decomp *p, uint32_t n)
+__device__ static inline uint64_t read_bits(struct sw842_param_decomp *p, uint32_t n)
 {
 	uint64_t value = p->buffer >> (WSIZE - n);
 	//value = 0; if (n <= 0)
@@ -92,8 +101,9 @@ __device__ inline uint64_t read_bits(struct sw842_param_decomp *p, uint32_t n)
 }
 
 #ifdef LIB842_CUDA_STRICT
-__device__ inline uint64_t get_index(struct sw842_param_decomp *p, uint8_t size,
-				     uint64_t index, uint64_t fsize)
+__device__ static inline uint64_t get_index(const struct sw842_param_decomp *p,
+					    uint8_t size,
+					    uint64_t index, uint64_t fsize)
 {
 	uint64_t offset;
 	uint64_t total = round_down(

src/ocl/decompress.cl
@@ -15,6 +15,17 @@ typedef ulong uint64_t;
 #define NULL 0L
 #endif
 
+#if defined(USE_MAYBE_COMPRESSED_CHUNKS) || defined(USE_INPLACE_COMPRESSED_CHUNKS)
+__constant static const uint8_t LIB842_COMPRESSED_CHUNK_MARKER[] =
+	LIB842_COMPRESSED_CHUNK_MARKER_DEF; // Defined at build time
+#endif
+
+#ifndef USE_INPLACE_COMPRESSED_CHUNKS
+#define RESTRICT_UNLESS_INLINE restrict
+#else
+#define RESTRICT_UNLESS_INLINE
+#endif
+
 #define ENABLE_ERROR_HANDLING
 
 struct sw842_param_decomp {
@@ -46,9 +57,9 @@ struct sw842_param_decomp {
 #define __round_mask(x, y) ((y)-1)
 #define round_down(x, y) ((x) & ~__round_mask(x, y))
 
-__constant uint16_t fifo_sizes[3] = { I2_FIFO_SIZE, I4_FIFO_SIZE, I8_FIFO_SIZE };
-__constant uint64_t masks[3] = { 0x000000000000FFFF, 0x00000000FFFFFFFF, 0xFFFFFFFFFFFFFFFF };
-__constant uint8_t dec_templates[26][4][2] = {
+__constant static const uint16_t fifo_sizes[3] = { I2_FIFO_SIZE, I4_FIFO_SIZE, I8_FIFO_SIZE };
+__constant static const uint64_t masks[3] = { 0x000000000000FFFF, 0x00000000FFFFFFFF, 0xFFFFFFFFFFFFFFFF };
+__constant static const uint8_t dec_templates[26][4][2] = {
 	// params size in bits
 	{ OP_DEC_D8, OP_DEC_N0, OP_DEC_N0, OP_DEC_N0 }, // 0x00: { D8, N0, N0, N0 }, 64 bits
 	{ OP_DEC_D4, OP_DEC_D2, OP_DEC_I2, OP_DEC_N0 }, // 0x01: { D4, D2, I2, N0 }, 56 bits
@@ -84,7 +95,7 @@ __constant uint8_t dec_templates[26][4][2] = {
 	{ OP_DEC_I8, OP_DEC_N0, OP_DEC_N0, OP_DEC_N0 }, // 0x19: { I8, N0, N0, N0 }, 8 bits
 };
 
-inline uint64_t bswap(uint64_t value)
+static inline uint64_t bswap(uint64_t value)
 {
 	return (uint64_t)((value & (uint64_t)0x00000000000000ff) << 56) |
 	       (uint64_t)((value & (uint64_t)0x000000000000ff00) << 40) |
@@ -96,7 +107,7 @@ inline uint64_t bswap(uint64_t value)
 	       (uint64_t)((value & (uint64_t)0xff00000000000000) >> 56);
 }
 
-inline uint64_t read_bits(struct sw842_param_decomp *p, uint32_t n)
+static inline uint64_t read_bits(struct sw842_param_decomp *p, uint32_t n)
 {
 	uint64_t value = p->buffer >> (WSIZE - n);
 	if (n == 0)
@@ -134,8 +145,8 @@ inline uint64_t read_bits(struct sw842_param_decomp *p, uint32_t n)
 	return value;
 }
 
-inline uint64_t get_index(struct sw842_param_decomp *p, uint8_t size,
-			  uint64_t index, uint64_t fsize)
+static inline uint64_t get_index(struct sw842_param_decomp *p, uint8_t size,
+				 uint64_t index, uint64_t fsize)
 {
 	uint64_t offset;
 	uint64_t total =
@@ -164,8 +175,8 @@ inline uint64_t get_index(struct sw842_param_decomp *p, uint8_t size,
 	return offset;
 }
 
-inline int decompress_core(__global const uint64_t *in, size_t ilen,
-			   __global uint64_t *out, size_t *olen)
+static inline int decompress_core(__global const uint64_t *RESTRICT_UNLESS_INLINE in, size_t ilen,
+				  __global uint64_t *RESTRICT_UNLESS_INLINE out, size_t *olen)
 {
 	struct sw842_param_decomp p;
 	p.ostart = p.out = out;
@@ -308,8 +319,10 @@ inline int decompress_core(__global const uint64_t *in, size_t ilen,
 	return 0;
 }
 
-__kernel void decompress(__global const uint64_t *in, ulong inOffset, __global const ulong *ilen,
-			 __global uint64_t *out, ulong outOffset, __global ulong *olen,
+__kernel void decompress(__global const uint64_t *RESTRICT_UNLESS_INLINE in,
+			 ulong inOffset, __global const ulong *ilen,
+			 __global uint64_t *RESTRICT_UNLESS_INLINE out,
+			 ulong outOffset, __global ulong *olen,
 			 ulong numChunks, __global int *returnValues)
 {
 	size_t chunk_num = get_global_id(0);
@@ -320,9 +333,10 @@ __kernel void decompress(__global const uint64_t *in, ulong inOffset, __global c
 	__global const uint64_t *my_in = in + (inOffset / 8) + ((CL842_CHUNK_STRIDE / 8) * chunk_num);
 
 #if defined(USE_MAYBE_COMPRESSED_CHUNKS) || defined(USE_INPLACE_COMPRESSED_CHUNKS)
-	if (my_in[0] != 0xd72de597bf465abe ||
-	    my_in[1] != 0x7670d6ee1a947cb2) { // = LIB842_COMPRESSED_CHUNK_MARKER
+	if (my_in[0] != ((__constant const uint64_t *)LIB842_COMPRESSED_CHUNK_MARKER)[0] ||
+	    my_in[1] != ((__constant const uint64_t *)LIB842_COMPRESSED_CHUNK_MARKER)[1]) {
 #ifdef USE_MAYBE_COMPRESSED_CHUNKS
+		// Copy uncompressed chunk from temporary input buffer to output buffer
 		for (size_t i = 0; i < CL842_CHUNK_SIZE; i++) {
 			my_out[i] = my_in[i];
 		}
@@ -333,6 +347,9 @@ __kernel void decompress(__global const uint64_t *in, ulong inOffset, __global c
 			returnValues[chunk_num] = 0;
 		return;
 	}
+
+	// Read compressed chunk size and skip to the beginning of the chunk
+	// (the end of the chunk matches the end of the input chunk buffer)
 	my_in += (CL842_CHUNK_SIZE - my_in[2]) / 8;
 #endif
 

src/serial/842-internal.h
@@ -2,74 +2,7 @@
 #define LIB842_SRC_SERIAL_842_INTERNAL_H
 
 #include "../common/842.h"
-#include "uthash/src/uthash.h"
-#include "uthash/src/utlist.h"
-
-#include <stdint.h>
 
 //#define DEBUG 1
-
-struct node2_el {
-	uint8_t index;
-	struct node2_el *next, *prev;
-};
-
-struct hlist_node2 {
-	uint16_t data;
-	struct node2_el *head;
-	UT_hash_handle hh;
-};
-
-struct node4_el {
-	uint16_t index;
-	struct node4_el *next, *prev;
-};
-
-struct hlist_node4 {
-	uint32_t data;
-	struct node4_el *head;
-	UT_hash_handle hh;
-};
-
-struct node8_el {
-	uint8_t index;
-	struct node8_el *next, *prev;
-};
-
-struct hlist_node8 {
-	uint64_t data;
-	struct node8_el *head;
-	UT_hash_handle hh;
-};
-
-struct sw842_param {
-	const uint8_t *in;
-	const uint8_t *instart;
-	uint64_t ilen;
-	uint8_t *out;
-	uint64_t olen;
-	uint8_t bit;
-	uint64_t data8[1];
-	uint32_t data4[2];
-	uint16_t data2[4];
-	int index8[1];
-	int index4[2];
-	int index2[4];
-	struct hlist_node8 *htable8;
-	struct hlist_node4 *htable4;
-	struct hlist_node2 *htable2;
-	uint64_t node8[1 << I8_BITS];
-	uint32_t node4[1 << I4_BITS];
-	uint16_t node2[1 << I2_BITS];
-};
-
-struct sw842_param_decomp {
-	const uint8_t *in;
-	uint8_t bit;
-	uint64_t ilen;
-	uint8_t *out;
-	const uint8_t *ostart;
-	uint64_t olen;
-};
 
 #endif // LIB842_SRC_SERIAL_842_INTERNAL_H

src/serial/842_compress.c
@@ -17,6 +17,8 @@
  */
 
 #include "842-internal.h"
+#include "uthash/src/uthash.h"
+#include "uthash/src/utlist.h"
 #include "kerneldeps.h"
 #include "../common/crc32.h"
 #include "../common/endianness.h"
@@ -24,8 +26,63 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <limits.h>
 #include <errno.h>
+
+struct node2_el {
+	uint8_t index;
+	struct node2_el *next, *prev;
+};
+
+struct hlist_node2 {
+	uint16_t data;
+	struct node2_el *head;
+	UT_hash_handle hh;
+};
+
+struct node4_el {
+	uint16_t index;
+	struct node4_el *next, *prev;
+};
+
+struct hlist_node4 {
+	uint32_t data;
+	struct node4_el *head;
+	UT_hash_handle hh;
+};
+
+struct node8_el {
+	uint8_t index;
+	struct node8_el *next, *prev;
+};
+
+struct hlist_node8 {
+	uint64_t data;
+	struct node8_el *head;
+	UT_hash_handle hh;
+};
+
+struct sw842_param {
+	const uint8_t *in;
+	const uint8_t *instart;
+	uint64_t ilen;
+	uint8_t *out;
+	uint64_t olen;
+	uint8_t bit;
+	uint64_t data8[1];
+	uint32_t data4[2];
+	uint16_t data2[4];
+	int index8[1];
+	int index4[2];
+	int index2[4];
+	struct hlist_node8 *htable8;
+	struct hlist_node4 *htable4;
+	struct hlist_node2 *htable2;
+	uint64_t node8[1 << I8_BITS];
+	uint32_t node4[1 << I4_BITS];
+	uint16_t node2[1 << I2_BITS];
+};
 
 /* By default, we allow compressing input buffers of any length, but we must
  * use the non-standard "short data" template so the decompressor can correctly

commit 728dd0a515d403e1acb291c2e4441c0a6a37671a

Fix inplace mode which was broken by e17a42c57aaa8e65e057a82c984878cca423fd2b ('Various general code quality improvements (possibly with performance impact').

src/ocl/cl842decompress.cpp
@@ -238,7 +238,8 @@ void CLDeviceDecompressor::buildProgram(
 	options << " -D CL842_CHUNK_STRIDE=" << m_inputChunkStride;
 	options << " -D EINVAL=" << EINVAL;
 	options << " -D ENOSPC=" << ENOSPC;
-	if (m_inputFormat == CLDecompressorInputFormat::MAYBE_COMPRESSED_CHUNKS) {
+	if (m_inputFormat == CLDecompressorInputFormat::MAYBE_COMPRESSED_CHUNKS ||
+	    m_inputFormat == CLDecompressorInputFormat::INPLACE_COMPRESSED_CHUNKS) {
 		options << " -D LIB842_COMPRESSED_CHUNK_MARKER_DEF={";
 		std::copy(std::begin(LIB842_COMPRESSED_CHUNK_MARKER),
 			  std::end(LIB842_COMPRESSED_CHUNK_MARKER),