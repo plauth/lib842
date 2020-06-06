3c3564e5a116c1d39eb22077fa591ce7ab431c42
Joan Bruguera <joanbrugueram@gmail.com>
2020-06-06 15:52:31 +0200

    test/test_decompress_boundscheck.c: Clarify ambiguous comment in the header.

--- test/test_decompress_boundscheck.c
+++ test/test_decompress_boundscheck.c
@@ -1,5 +1,6 @@
-/* Test for regression in the original software decompressor implementation
-   in the Linux Kernel (https://lkml.org/lkml/2020/6/5/727). Description:
+/* Test for regression present in the original software decompressor
+   implementation in the Linux Kernel (https://lkml.org/lkml/2020/6/5/727),
+   which also formerly affected our implementations. Description:
 
    The software 842 decompressor receives, through the initial value of the
    'olen' parameter, the capacity of the buffer pointed to by 'out'. If this
